ca7b4861eb1af273a458ebee5f23f93fd6f29a4d
itsirovasilis <ioannistsirovasilis@gmail.com>
2020-10-07 11:31:19 +0300

    Added config file default.json Added file for db context

---
+++ models/context.js
@@ -0,0 +1,31 @@
+const mongodb = require('mongodb');
+const config = require('config');
+const connectionString = config.get('db.connectionString');
+const dbName = config.get('db.database');
+
+const context = function() {
+    // connect to the database and return connection
+    async function connect() {
+        try {
+            let con = await mongodb.MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true });
+            return con.db(dbName);            
+        } catch (error) {
+            return error;
+        }    
+    }
+
+    // this is the exported function that gets the connection
+    async function get() {
+        try {
+            return await connect();
+        } catch (error) {
+            return error;
+        }  
+    }
+    
+    return {
+        get: get
+    }
+}
+
+module.exports = context();
